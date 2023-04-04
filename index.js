13b582eb9331a82433d355242deb7cc4388d217d
omiershad <omiershad@gmail.com>
2023-04-04 12:42:58 -0700

    process of adding boxes

---
+++ index.js
@@ -0,0 +1,14 @@
+const containerDiv = document.getElementsByClassName("container");
+
+function createGrids () {
+    const p = document.createElement('p');
+    p.classList.add("boxes");
+    containerDiv.appendChild(p);
+}
+
+
+function createBoxes () {
+    for (i = 0; i < 256; ii++) {
+
+    }
+}
